8e917bc1ddd7689bb83ded6e57d201408eb3157a
laravell2004 <agungnugroho16@sma.belajar.id>
2023-02-27 16:15:04 +0700

    memindahkan folder assets

--- pages/detail.tsx
+++ pages/detail.tsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from "react"
 import Navbar from "./../components/organ/Navbar";
-import DetailImage from './assets/detail.png'
+import DetailImage from './../assets/detail.png'
 import Image from "next/image";
 import Head from "next/head";
 import Button from "./../components/atom/Button";
